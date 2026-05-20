be00abb833c566412e845e9ea0474419b6c7f445
Pravin Junghare <pravinjunghare01@gmail.com>
2026-05-20 14:22:04 +0530

    Added ProductInfoPage_UseOfHashMap_LinkedHashMap_TreeMap_SoftAssert_HardAssert_DataProvider

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,11 +5,13 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.Driverfactory;
 import com.qa.opencart.pages.AccountsPage;
 import com.qa.opencart.pages.LoginPage;
 import com.qa.opencart.pages.ProductInfoPage;
+//import com.qa.opencart.pages.RegisterPage;
 import com.qa.opencart.pages.SearchResultPage;
 
 public class BaseTest {
@@ -24,15 +26,18 @@ public class BaseTest {
 	protected AccountsPage accountsPage;
 	protected SearchResultPage searchPage;
 	protected ProductInfoPage productInfoPage;
+	protected SoftAssert softAssert;
+	// protected RegistrationPage registrationPage;
 
 	@BeforeTest
 	public void setUp() {
 		df = new Driverfactory();// created driver factory object
-		prop=df.initProp();
-		driver = df.initDriver(prop);// to call initDriver method to get driver
+		prop = df.initProp();// df.initProp() will give prop reference
+		// driver = df.initDriver("chrome");// to call initDriver method to get driver
+		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
+		softAssert = new SoftAssert();
 	}
-
 	@AfterTest
 	public void teardown() {
 		driver.quit();
